c633f867aa4feda5843992ab8901d9fc5ffc9fae
Pummarin Jomkoa <1388309+kittenlinux@users.noreply.github.com>
2021-09-22 11:49:22 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -20,12 +20,6 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
 
 import { mainListItems } from './listItems';
 import Title from './Title';
